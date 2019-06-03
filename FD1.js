d895da02d1443262e6ed29b908ffc2ffb7fc5325
Chris Martin <chris.bizunit34@gmail.com>
2019-06-03 15:10:22 -0400

    added ability to catch errors

--- FD1.js
+++ FD1.js
@@ -1,11 +1,20 @@
+/* eslint-disable indent */
 /* eslint-disable no-console */
 // eslint-disable-next-line strict
 'use strict';
-var getYearOfBirth = function(age){
-  return 2019 - age;
+var getYearOfBirth = function(age){ 
+    if (age < 0) {
+        throw new Error('Age can not be negative');
+    } 
+    return 2019 - age;  
 };
 var createGreeting = function(name, age){
   return `Hi, my name is ${name} and I am ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 };
-createGreeting();
-console.log(createGreeting);
+try {
+   const greeting1 = createGreeting();
+   console.log(greeting1);
+}
+catch(e) {
+    alert(e.message);
+}
