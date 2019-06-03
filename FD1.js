a11dfb77e9f501287441478e5ac3a15391e6215d
David Queen <dsqjr@msn.com>
2019-06-03 14:57:17 -0400

    moved age calculation to outside function

--- FD1.js
+++ FD1.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line strict
 'use strict';
+var getYearOfBirth = function(age){
+  return 2019 - age;
+};
 var createGreeting = function(name, age){
-  const yearOfBirth = 2016 - age;
-  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 };
 createGreeting();
 console.log(createGreeting);
