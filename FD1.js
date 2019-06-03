a11ffae30a57d19706bb1c51c594bfaf2a10d45d
David Queen <dsqjr@msn.com>
2019-06-03 14:50:08 -0400

    added parameters

--- FD1.js
+++ FD1.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line strict
 'use strict';
-var createGreeting = function(){
-  return 'Hi, my name is Chris and I am 29 years old';
+var createGreeting = function(name, age){
+  return `Hi, my name is ${name} and I am ${age} years old`;
 };
 createGreeting();
 console.log(createGreeting);
