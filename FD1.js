f0e484c47607ca2130515d7e85c72b67146e6428
Chris Martin <chris.bizunit34@gmail.com>
2019-06-03 14:44:31 -0400

    changed to return and consoled the value

--- FD1.js
+++ FD1.js
@@ -2,6 +2,7 @@
 // eslint-disable-next-line strict
 'use strict';
 var createGreeting = function(){
-  console.log('Hi, my name is Chris and I am 29 years old');
+  return 'Hi, my name is Chris and I am 29 years old';
 };
 createGreeting();
+console.log(createGreeting);
