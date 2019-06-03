0dc076114b5f7f6b31f5d9714ece8f6fa8691dea
David Queen <dsqjr@msn.com>
2019-06-03 14:42:05 -0400

    wraped in a function

--- FD1.js
+++ FD1.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
 // eslint-disable-next-line strict
 'use strict';
-
-console.log('Hi, my name is Chris and I am 29 years old');
+var createGreeting = function(){
+  console.log('Hi, my name is Chris and I am 29 years old');
+};
+createGreeting();
