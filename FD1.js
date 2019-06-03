d7941d878fdd81e2f0fcb5ae9759b513da975252
Chris Martin <chris.bizunit34@gmail.com>
2019-06-03 14:53:59 -0400

    added birth year

--- FD1.js
+++ FD1.js
@@ -2,7 +2,8 @@
 // eslint-disable-next-line strict
 'use strict';
 var createGreeting = function(name, age){
-  return `Hi, my name is ${name} and I am ${age} years old`;
+  const yearOfBirth = 2016 - age;
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
 };
 createGreeting();
 console.log(createGreeting);
