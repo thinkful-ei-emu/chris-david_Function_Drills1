a69ef18ea51dbe800450c13dbfd3ded9ff0949bc
David Queen <dsqjr@msn.com>
2019-06-03 15:35:33 -0400

    automated commit

--- FD1.js
+++ FD1.js
@@ -13,17 +13,17 @@ var getYearOfBirth = function(age){
 };
 var createGreeting = function(name, age){
   if(name === undefined || age === undefined){
-      throw new Error('arguments not vaild');
+      throw new Error('No Arguments found');
   }
-  if(isNaN(age) || typeof(name) === typeof('name')){
-      throw new TypeError('Arguments not valid');
+  if(isNaN(age) || typeof(name) === 'name' ){
+      throw new TypeError('Arguments are not the right type');
   }
   return `Hi, my name is ${name} and I am ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 };
 try {
-   const greeting1 = createGreeting();
+   const greeting1 = createGreeting('David', 27);
    console.log(greeting1);
 }
 catch(e) {
-    alert(e.message);
+    log(e.message);
 }
